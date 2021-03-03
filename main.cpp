e388bd1d8148b113f3dde4859b861a0c5c63d315
BrianLevin <bml201095@gmail.com>
2021-03-03 12:48:07 -0500

    made a function which will return a new array

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 #include <iostream>
 
 using namespace std;
+// return pointer address to an integer,
+// the integers and pointers to the array are constant just changing the new array 
+
+int *apply_all(const int *const arr1, size_t size1, const int * const arr2, size_t size2) {
+
+int *new_array {};
+new_array = new int[size1 * size2]; // neww array size
+
+int position {0};
+    for (size_t i{0}; i < size2; ++i) {
+        for (size_t j{0}; j< size1; ++j) {
+            new_array[position] = arr1[j] * arr2[i];
+            ++position;
+        }
+    }
+    return new_array;
+}
 
 // function to display array values
 void print (const int *const arr, size_t size) // constant poointer cant change what its pointing to display function
