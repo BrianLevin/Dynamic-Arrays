df34cba82bc1e4f3a9c8e1ae57de37c33e8ccf79
BrianLevin <bml201095@gmail.com>
2021-03-03 12:28:41 -0500

    created a function which will display the aray one values and size

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 
 using namespace std;
 
+// function to display array values
+void print (const int *const arr, size_t size) // constant poointer cant change what its pointing to display function
+cout << "[ ";
+for(size_t i{0}; i<size; ++i)
+cout << arr[i] << " ";
+cout << "]"
+cout << endl;
 
 int main(){
 
-
+ 
 
     return 0;
 }
